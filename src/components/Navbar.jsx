94e876224575e88de7e5c0218ed2b1d191c9ce62
Millad Ayoubi <Milladayoubi@gmail.com>
2023-02-19 23:15:10 -0500

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Button, Menu, Typography, Avatar } from 'antd';
-import { Link } from 'react-rounter-dom'
+import { Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BuldOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+ 
 
 
 
